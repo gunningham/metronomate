1fa92cb79ced1efa0eec3bc0a0000ad6dff9e5f7
Alex Gunningham <alex.g@mmtdigital.co.uk>
2019-10-16 08:39:35 +0100

    Begin implementing Howl

--- src/components/Metronome/Metronome.js
+++ src/components/Metronome/Metronome.js
@@ -1,5 +1,6 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import queryString from 'query-string'
+import { Howl } from 'howler'
 import Button from '../Button'
 import TempoSlider from '../TempoSlider'
 import { DEFAULT_TEMPO } from '../../constants'
@@ -7,10 +8,10 @@ import { fetchEntries } from '../../services/contentful'
 import styles from './Metronome.scss'
 
 const Metronome = () => {
-  const [activeBeatUrl, setActiveBeatUrl] = useState(null)
+  const [activeBeatUrl, setActiveBeatUrl] = useState('')
   const [genre, setGenre] = useState('rock')
   const [beats, setBeats] = useState([])
-  const audioInput = useRef(null)
+  const howl = new Howl({ src: [activeBeatUrl], loop: true })
 
   const onTempoChange = tempo => {
     if (activeBeatUrl) audioInput.current.playbackRate = tempo / DEFAULT_TEMPO
@@ -35,6 +36,13 @@ const Metronome = () => {
     if (!beats.length) getBeats()
   })
 
+  const handleBeatChange = url => {
+    if (activeBeatUrl) howl.stop()
+    howl.src = [url]
+    howl.play()
+    setActiveBeatUrl(url)
+  }
+
   return (
     <div className={styles.wrap}>
       <div className={styles.buttons}>
@@ -43,12 +51,11 @@ const Metronome = () => {
             key={beat.id}
             isActive={activeBeatUrl === beat.url}
             text={beat.text}
-            onClick={() => setActiveBeatUrl(beat.url)}
+            onClick={() => handleBeatChange(beat.url)}
           />
         ))}
       </div>
       <TempoSlider onChange={onTempoChange} />
-      <audio ref={audioInput} src={activeBeatUrl} autoPlay loop />
     </div>
   )
 }
