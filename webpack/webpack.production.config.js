c96755b95b9f4572f97e1c589e028446784f0baf
Alex Gunningham <alex.g@mmtdigital.co.uk>
2020-01-26 14:40:52 +0000

    Define env vars in prod build

--- webpack/webpack.production.config.js
+++ webpack/webpack.production.config.js
@@ -1,5 +1,15 @@
 const base = require('./webpack.base.config')
+const webpack = require('webpack')
 
 module.exports = {
-  ...base
+  ...base,
+  plugins: [
+    ...base.plugins,
+    new webpack.DefinePlugin({
+      'process.env': {
+        REACT_APP_CONTENTFUL_SPACE: process.env.REACT_APP_CONTENTFUL_SPACE,
+        REACT_APP_CONTENTFUL_ACCESS_TOKEN: process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN
+      }
+    })
+  ]
 }
