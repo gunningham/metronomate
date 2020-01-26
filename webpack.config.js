aac7a3ae68937d96f4e331dd2a89d1bfabc9677a
Alex Gunningham <alex.g@mmtdigital.co.uk>
2020-01-26 14:57:39 +0000

    remove dotEnv

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,46 @@
-require('dotenv').config()
-const { NODE_ENV = 'production' } = process.env
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-module.exports = NODE_ENV === 'development'
-  ? require('./webpack/webpack.development.config')
-  : require('./webpack/webpack.production.config')
+module.exports = {
+  entry: './src/app',
+  output: {
+    filename: 'bundle.js'
+  },
+  node: {
+    fs: 'empty'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              modules: {
+                mode: 'local',
+                localIdentName: '[local]--[hash:base64:5]'
+              }
+            }
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: ('./index.html')
+    })
+  ]
+}
