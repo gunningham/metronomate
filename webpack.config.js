ce0dff8bb81b69180b582d52a03d0d4e32883161
Alex Gunningham <alex.g@mmtdigital.co.uk>
2020-01-26 20:12:04 +0000

    Add svg loader

--- webpack.config.js
+++ webpack.config.js
@@ -39,6 +39,20 @@ module.exports = {
             }
           }
         ]
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'babel-loader'
+          },
+          {
+            loader: 'react-svg-loader',
+            options: {
+              jsx: true
+            }
+          }
+        ]
       }
     ]
   },
