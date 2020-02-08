aef941ad99ed2cfe5501192fcfc7568f0a250b83
Alex Gunningham <alex.g@mmtdigital.co.uk>
2020-02-08 14:21:38 +0000

    Add insta icon to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Logo from '../../resources/svgs/logo.svg'
+import InstaIcon from '../../resources/svgs/instagra-icon.svg'
 import styles from './Header.scss'
 
 const Header = () => (
   <header className={styles.header}>
     <Logo />
+    <InstaIcon />
   </header>
 )
 
