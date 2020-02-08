e7c038104a3ec7784f195049c57067941bbaa47c
Alex Gunningham <alex.g@mmtdigital.co.uk>
2020-02-08 14:22:05 +0000

    Allow linkTo in button component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import { Link } from 'react-scroll'
 import styles from './Button.scss'
 
-const Button = ({ text, onClick, isActive }) => {
+const Button = ({ text, onClick, isActive, linkTo }) => {
   const classNames = `${styles.button} ${isActive ? styles.active : ''}`
 
   return (
     <button className={classNames} onClick={onClick}>
-      {text}
+      {!linkTo && text}
+      {linkTo && <Link smooth duration={300} to='section-2'>{text}</Link>}
     </button>
   )
 }
